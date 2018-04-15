d238abfccbe098fefeb3587334cd541001f1e221
Vasilev07 <georgi_vasilev07@abv.bg>
2018-04-15 17:21:59 +0300

    added bcrypt pack and crypto controller for hash passwords

--- back-end/configuration/authentication.js
+++ back-end/configuration/authentication.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const passport = require('passport');
 const Strategy = require('passport-local').Strategy;
 
+const Crypto = require('../controllers/cryptography-controller');
 
 const users = [{
     username: 'Pesho',
@@ -17,11 +18,14 @@ const init = (app) => {
         (username, password, done) => {
             const user = users.find((dbUser) => dbUser.username === username);
 
+            const comparePasswords = new Crypto().comparePasswords;
+
             if (!user) {
                 return done(null, false, {
                     message: 'Incorrect username.',
                 });
             }
+            const checkIfPassMatch = comparePasswords(password, user.password);
             if (user.password !== password) {
                 return done(null, false, {
                     message: 'Incorrect password.',
