10e95bb558372f128c64d132ad28eefc08fe1890
Vasilev07 <georgi_vasilev07@abv.bg>
2018-04-15 13:32:42 +0300

    server configuration

---
+++ back-end/configuration/express.js
@@ -0,0 +1,25 @@
+/* globals __dirname*/
+// const path = require('path');
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const morgan = require('morgan');
+
+// const init = (app) => {
+//     if (typeof app.use !== 'function' ||
+//         typeof app.set !== 'function') {
+//         throw new Error('Invalid app');
+//     }
+
+//     app.use(bodyParser.urlencoded({
+//         extended: true,
+//     }));
+
+//     app.use('/static', express.static(path.join(__dirname, '../../public')));
+//     // not sure if we will use public att all
+//     app.use(morgan('combined'));
+// };
+
+// module.exports = {
+//     init,
+// };
---
+++ back-end/configuration/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable*/
+const port = process.env.PORT || 3001;
+/* eslint-enable*/
+const secret = `A small yellow rabbit hops all 
+day in a black orchard with huge butterflies passing by.`;
+module.exports = {
+    port,
+    secret,
+};
