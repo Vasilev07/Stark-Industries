237d8ebe7b4635b3d62fc2fa0e26dbaf22c206e6
Vasilev07 <georgi_vasilev07@abv.bg>
2018-04-15 16:50:52 +0300

    createted initial  authentication with passport and mocked data

--- back-end/configuration/authentication.js
+++ back-end/configuration/authentication.js
@@ -1,1 +1,56 @@
-// use passport here
+const config = require('../configuration');
+
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+
+
+const users = [{
+    username: 'Pesho',
+    password: '123456',
+}];
+
+const init = (app) => {
+    passport.use(new Strategy(
+        (username, password, done) => {
+            const user = users.find((dbUser) => dbUser.username === username);
+
+            if (!user) {
+                return done(null, false, {
+                    message: 'Incorrect username.',
+                });
+            }
+            if (user.password !== password) {
+                return done(null, false, {
+                    message: 'Incorrect password.',
+                });
+            }
+            return done(null, user);
+        }
+    ));
+
+    passport.serializeUser((user, done) => {
+        done(null, user.username);
+    });
+
+    passport.deserializeUser((username, done) => {
+        const user = users.find((dbUser) => dbUser.username === username);
+        if (!user) {
+            return done(new Error('invalid user'));
+        }
+        return done(null, user);
+    });
+
+    app.use(cookieParser());
+    app.use(session({
+        secret: config.secret,
+    }));
+    app.use(passport.initialize());
+    app.use(passport.session());
+};
+
+module.exports = {
+    init,
+};
