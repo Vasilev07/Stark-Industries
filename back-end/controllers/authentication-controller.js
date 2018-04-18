d1ae8a8c3786c1c84c3f39975e03f1f3a5bb979e
Vasilev07 <georgi_vasilev07@abv.bg>
2018-04-18 14:19:18 +0300

    create authentication with jwt with Vladi

---
+++ back-end/controllers/authentication-controller.js
@@ -0,0 +1,72 @@
+const bcrypt = require('bcrypt');
+const uuid = require('uuid/v4');
+const jwt = require('jwt-simple');
+const moment = require('moment');
+
+const config = require('../configuration/config');
+
+class AuthenticationController {
+    constructor(data) {
+        this.data = data;
+    }
+
+    async register(req, res) {
+        const userFound = await this.data.users
+            .getByValue('userName', req.body.userName);
+        if (!userFound) {
+            const user = {
+                userName: req.body.userName,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                email: req.body.email,
+                password: req.body.password,
+                roleId: 1,
+            };
+            const hashedPassword = bcrypt.hash(req.body.password, 10);
+            user.password = await hashedPassword;
+            await this.data.users.create(user);
+            console.log(user);
+            res.status(200).end();
+        } else {
+            res.status(401).send({
+                err: 'User already exists',
+            });
+        }
+    }
+
+    async login(req, res) {
+        console.log(req.body);
+        const userFound = await this.data.users
+            .getByValue('userName', req.body.userName);
+            console.log(userFound);
+        if (userFound) {
+            bcrypt
+                .compare(req.body.password,
+                    userFound.password, (err, response) => {
+                        if (response) {
+                            const expire = moment(new Date())
+                                .add(config.JWT_EXPIRE_TIME, 'minutes').unix();
+                            const payload = {
+                                sub: userFound.id,
+                                email: userFound.email,
+                                role: userFound.role,
+                                exp: expire,
+                                iss: config.JWT_ISS,
+                            };
+                            const secret = config.JWT_SECRET;
+                            const token = jwt.encode(payload, secret);
+
+                            res.status(200).send({
+                                token,
+                            });
+                        } else {
+                            res.status(401).send({
+                                err: 'User not found',
+                            });
+                        }
+                    });
+        }
+    }
+}
+
+module.exports = AuthenticationController;
--- back-end/routes/authentication-routes.js
+++ back-end/routes/authentication-routes.js
@@ -2,42 +2,26 @@ const passport = require('passport');
 const {
     Router,
 } = require('express');
-const UsersController = require('../controllers/users-controller');
+const AuthenticationController =
+    require('../controllers/authentication-controller');
 
 const init = (app, data) => {
-    // // const router = new Router();
-    // const controller = new UserController(data);
-    // app.get('/login', (req, res) => {
-    //     res.render('../views/home');
-    // });
-    // app.post('/login', (req, res, next) => {
-    //     passport.authenticate('local', (err, user, info) => {
-    //         if (err) {
-    //             return next(err);
-    //         }
-    //         if (!user) {
-    //             return res.status(300).json(info);
-    //         }
-    //         return req.login(user, (error) => {
-    //             if (error) {
-    //                 return res.status(300).json(info);
-    //             }
-    //             return res.redirect('/');
-    //         })(req, res, next);
-    //     });
-    // });
-    // app.get('/logout', (req, res) => {
-    //     req.logout();
-    //     req.redirect('/');
-    // });
-    // app.get('/register', (req, res) => {
-    //     res.render('../views/home');
-    // });
-    // app.post('/register', async (req, res) => {
-    //     const userObj = req.body;
-    //     await controller.users.createNewUser(userObj, userObj.role);
-    //     res.redirect('/login');
-    // });
+    const router = new Router();
+    const controller = new AuthenticationController(data);
+    app.use('', router);
+    router
+        .get('/login', (req, res) => {
+            res.render('../views/home.pug');
+        })
+        .get('/register', (req, res) => {
+            res.render('../views/home.pug');
+        })
+        .post('/login', async (req, res) => {
+            await controller.login(req, res);
+        })
+        .post('/register', async (req, res) => {
+            await controller.register(req, res);
+        });
 };
 
 module.exports = {
