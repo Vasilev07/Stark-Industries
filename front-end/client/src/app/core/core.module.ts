0297a96f45f43162fe540f8f1c16b8da101aed5a
Vladimir Tumbev <vladimirgtumbev@gmail.com>
2018-04-24 20:32:02 +0300

    merge

--- front-end/client/src/app/core/core.module.ts
+++ front-end/client/src/app/core/core.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 
-import { CareersService } from './careers.service';
 import { AuthService } from './auth.service';
+import { CareersService } from './careers.service';
 
 @NgModule({
   providers: [
     {provide: CareersService, useClass: CareersService},
     {provide: AuthService, useClass: AuthService},
-  ]
+  ],
 })
 export class CoreModule { }
